fc4602e3c9f84364e8c4596440f7eee330d8e550
Soumya Mittal <soumyamittalrgh@gmail.com>
2024-07-05 23:52:40 +0530

    env update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,8 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs.send(
+    emailjs.send( 
+      
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       
